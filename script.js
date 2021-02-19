4f123f3bc08484d5c153c0b1e30344ec8716dbe7
Josue <josair21@gmail.com>
2021-02-19 14:30:23 -0500

    about and project interaction is added

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-document.querySelector('.nav-main').addEventListener('click',() => {
+document.querySelector('.menu').addEventListener('click',() => {
     document.querySelector('.categories').classList.toggle('show');
+});
+document.querySelector('.about_btn').addEventListener('click',() => {
+     document.querySelector('.showcase').classList.add('show');
+     document.querySelector('.project').classList.remove('show');
+     console.log("about")
+ });
+ document.querySelector('.project_btn').addEventListener('click',() => {
+    document.querySelector('.showcase').classList.remove('show');
+    document.querySelector('.project').classList.add('show');
+    console.log("about")
 });
 ScrollReveal().reveal('.container',{delay: 1000});
 ScrollReveal().reveal('.news-cards',{delay:500});
