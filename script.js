8188c17844684b355ab34fedea2e21440cb5d45f
Josue Lopez <josair21@gmail.com>
2021-02-19 18:18:26 -0500

    forgot add all files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-ScrollReveal().reveal('.container',{delay: 1000});
+ScrollReveal().reveal('.header',{delay: 1000});
 ScrollReveal().reveal('.news-cards',{delay:500});
 ScrollReveal().reveal('.cards-banner-one',{delay:500});
 ScrollReveal().reveal('.cards-banner-two',{delay:500});
@@ -7,13 +7,13 @@ document.querySelector('.menu').addEventListener('click',() => {
 });
 document.querySelector('.about_btn').addEventListener('click',() => {
     
-     document.querySelector('.showcase').classList.add('show');
+     document.querySelector('.about').classList.add('show');
      document.querySelector('.project').classList.remove('show');
      console.log("about_btn")
      
  });
  document.querySelector('.project_btn').addEventListener('click',() => {
-    document.querySelector('.showcase').classList.remove('show');
+    document.querySelector('.about').classList.remove('show');
     document.querySelector('.project').classList.add('show');
     console.log("project_btn")
 });
