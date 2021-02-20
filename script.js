192ecda13b06fc692812931dc2156bcbd4bde48c
Josue <josair21@gmail.com>
2021-02-20 13:04:26 -0500

    improve display all in one page

--- script.js
+++ script.js
@@ -27,3 +27,8 @@ document.querySelector('.temperature_arduino_title').addEventListener('click',()
     document.querySelector('.temperature_arduino_photo').classList.toggle('show');
     console.log("temperature_arduino_btn")
 });
+document.querySelector('.window_bt_title').addEventListener('click',() => {
+    document.querySelector('.window_bt_description').classList.toggle('show');
+    document.querySelector('.window_bt_photo').classList.toggle('show');
+    console.log("window_bt_btn")
+});
